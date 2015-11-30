789521dd744a840182eba41e9a93810f1ca5a9a4
kgaurav7 <kgaurav7@gatech.edu>
2015-11-30 04:15:20 +0000

    Added bleach support for HTML sanitization

--- catalog/main/views.py
+++ catalog/main/views.py
@@ -13,6 +13,7 @@ from forms import EditForm, AddForm
 from flask import abort, redirect, url_for
 from flask.ext.login import login_required, current_user
 from catalog import db
+import bleach
 
 
 @main.route('/')
@@ -49,8 +50,8 @@ def editItem(id):
 
     form = EditForm(id=id, name=item.name, description=item.description)
     if form.validate_on_submit():
-        item.name = form.name.data
-        item.description = form.description.data
+        item.name = bleach.clean(form.name.data)
+        item.description = bleach.clean(form.description.data)
 
         db.session.add(item)
         db.session.commit()
@@ -75,8 +76,8 @@ def addItem():
     if form.validate_on_submit():
         print current_user.get_id()
         print form.category.data
-        item = Item(name=form.name.data,
-                    description=form.description.data,
+        item = Item(name=bleach.clean(form.name.data),
+                    description=bleach.clean(form.description.data),
                     owner_id=int(current_user.get_id()),
                     category_id=int(form.category.data))
 
